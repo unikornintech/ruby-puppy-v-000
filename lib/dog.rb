7ebe5c904290d373b8276a0d411c4d3ac5296d3f
Lauren Days <lmchambers0420@gmail.com>
2019-02-06 21:42:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+   def self.clear_all
+     @@all = []
+   end 
+   
+   def self.all 
+     @@all.map do |dog|
+       puts dog.name 
+     end
+   end 
+   
+end
