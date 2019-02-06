2426cb6842815256c6559f31e00ffe8e1b91f5e4
Lauren Days <lmchambers0420@gmail.com>
2019-02-06 21:27:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,20 @@ class Dog
   
   @@all = []
   
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all =[]
+
+  end
+
+  def self.all
+    @@all.map do |dog|
+      puts dog.name
+    end
+
+  end
 end 
